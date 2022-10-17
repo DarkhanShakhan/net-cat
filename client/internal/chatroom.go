09b872ccd20a9c429adb0652f72af4206084c3eb
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2022-10-17 15:49:17 +0600

    adds chat

--- client/internal/chatroom.go
+++ client/internal/chatroom.go
@@ -1,7 +1,9 @@
 package internal
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 
 	"github.com/jroimartin/gocui"
 )
@@ -17,17 +19,51 @@ func (u *User) StartChat(g *gocui.Gui, v *gocui.View) error {
 	query := fmt.Sprintf("/join %s\n", v.Name())
 	u.conn.Write([]byte(query))
 	maxX, maxY := g.Size()
-	left, right := (maxX-15)/2, maxX-((maxX-15)/2)
-	top, _ := (maxY-18)/2, maxY-((maxY-18)/2)
-	if v, err := g.SetView("input", left-15, top, right+15, top+7); err != nil {
+	if _, err := g.SetView("output", 2, 2, maxX-2, maxY-8); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		for i := 0; i < 2; i++ {
-			msg, _ := u.reader.ReadString('\n')
-			fmt.Fprint(v, msg)
+		// go u.read(v)
+	}
+	out, _ := g.View("output")
+	go u.read(out)
+	if tr, err := g.SetView("input", 2, maxY-7, maxX-2, maxY-4); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
 		}
+		tr.Editable = true
+	}
+	if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, u.sendMessage); err != nil {
+		log.Panicln(err)
+	}
+	g.SetCurrentView("input")
+	return nil
+}
 
+func (u *User) sendMessage(g *gocui.Gui, v *gocui.View) error {
+	msg := v.Buffer()
+	if msg != "" {
+		u.conn.Write([]byte(msg + "\n"))
+		// u.reader.ReadString(':')
+		out, _ := g.View("output")
+		fmt.Fprint(out, msg)
+		v.Clear()
+		v.SetCursor(0, 0)
 	}
 	return nil
+}
+
+func (u *User) read(v *gocui.View) {
+	scanner := bufio.NewScanner(u.conn)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		if msg[len(msg)-2:] == "]:" {
+			continue
+		}
+		fmt.Fprintln(v, msg)
+	}
+	// for {
+	// 	msg, _ := u.reader.ReadString('\n')
+	// 	fmt.Fprint(v, msg)
+	// }
 }
